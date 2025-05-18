8267b2e35be454eb377a6977899715c66f3cd86d
Saahil <neon@saahild.com>
2025-05-18 19:34:42 -0400

    update:todays work

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 // urls to scrape  https://hacker-news.firebaseio.com/v0/topstories.json (only ID's) ( please slice to top 30)
 // https://api.hackerwebapp.com/news?page=0 full detail more accurate
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    // logLevel: 'debug',
+});
+
+app.command('/hn-subscribe', async ({ ack, body, client, context, say }) => {
+    if(ack) ack()
+        
+})
+
+app.command('/hn-unsubscribe', async ({ ack, body, client, context, say }) => {
+if (ack) ack()
+})
+
