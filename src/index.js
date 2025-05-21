a4e3f6cf74bd58d180457a3be361bc695a9deaed
Saahil <neon@saahild.com>
2025-05-21 18:58:59 -0400

    update:a

--- src/index.js
+++ src/index.js
@@ -7,17 +7,40 @@ const db = new QuickDB();
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    // signingSecret: process.env.SLACK_SIGNING_SECRET,
     socketMode: true,
-    // logLevel: 'debug',
+//    logLevel: 'debug',
 });
 
 app.command('/hn-subscribe', async ({ ack, body, client, context, say }) => {
     if(ack) ack()
-        
+    const args = body.text.split(' ');
+    const channel = args[1];
+    const idToTrack = args[0]
+    console.log(idToTrack, channel)
+  await   db.push(body.user_id, {
+        channel: channel || body.user_id,
+        id: idToTrack
+    })
+await say({
+    text: `Subscribed to ${idToTrack}${channel ? ` in ${channel}` : ''}.`,
+    ephemeral: true,
+});
 })
-
 app.command('/hn-unsubscribe', async ({ ack, body, client, context, say }) => {
 if (ack) ack()
+    const args = body.text.split(' ');
+    const channel = args[1];
+    const idToTrack = args[0]
+    console.log(idToTrack, channel)
+    await db.remove(body.user_id, {
+        channel: channel || body.user_id,
+        id: idToTrack
+    })
+    await say({
+        text: `Unsubscribed from ${idToTrack}${channel ? ` in ${channel}` : ''}.`,
+        ephemeral: true,
+    });
 })
 
+app.start()
