191b86691444da0ec68228b9edde6b41acaa5855
Saahil <neon@saahild.com>
2025-05-23 19:32:33 -0400

    update: move code around

--- src/index.js
+++ src/index.js
@@ -11,38 +11,8 @@ const app = new App({
   socketMode: true,
   //    logLevel: 'debug',
 });
-db.all().then((e) => console.log(e[0]));
+// db.all().then((e) => console.log(e[0]));
 
-app.command("/hn-subscribe", async ({ ack, body, client, context, say }) => {
-  if (ack) ack();
-  const args = body.text.split(" ");
-  const channel = args[1];
-  const idToTrack = args[0];
-  console.log(idToTrack, channel);
-  await db.push(body.user_id, {
-    channel: channel || body.user_id,
-    id: idToTrack,
-  });
-  await say({
-    text: `Subscribed to ${idToTrack}${channel ? ` in ${channel}` : ""}.`,
-    ephemeral: true,
-  });
-});
-app.command("/hn-unsubscribe", async ({ ack, body, client, context, say }) => {
-  if (ack) ack();
-  const args = body.text.split(" ");
-  const channel = args[1];
-  const idToTrack = args[0];
-  console.log(idToTrack, channel);
-  await db.remove(body.user_id, {
-    channel: channel || body.user_id,
-    id: idToTrack,
-  });
-  await say({
-    text: `Unsubscribed from ${idToTrack}${channel ? ` in ${channel}` : ""}.`,
-    ephemeral: true,
-  });
-});
 app.event("app_home_opened", async ({ event, context, say }) => {
   await app.client.views.publish({
     user_id: event.user,
@@ -71,4 +41,5 @@ app.event("app_home_opened", async ({ event, context, say }) => {
     },
   });
 });
+require("./cmds")(app, db);
 app.start();
